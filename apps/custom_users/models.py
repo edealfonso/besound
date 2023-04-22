d5192b6a08084f8654f0f69fea06980bfde272fd
edealfonso <elsa.de.alfonso@gmail.com>
2023-04-23 01:33:21 +0200

    users models ok

--- apps/custom_users/models.py
+++ apps/custom_users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from apps.custom_users.manager import CustomUserManager
 from solo.models import SingletonModel
 
 class LoginPage(SingletonModel):
@@ -25,9 +26,17 @@ class SignupPage(SingletonModel):
         verbose_name = "Login Page"
 
 class CustomUser(AbstractUser):
+    # username = None
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     birth_date = models.DateField()
     nationality = models.CharField(max_length=255)
     email = models.EmailField(unique = True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'birth_date', 'nationality']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
