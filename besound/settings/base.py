75de13c6ca0b36e292884d8672299980c981cae1
edealfonso <elsa.de.alfonso@gmail.com>
2023-06-09 23:43:03 +0200

    allow all

--- besound/settings/base.py
+++ besound/settings/base.py
@@ -171,8 +171,7 @@ REST_FRAMEWORK = {
 }
 
 # Allowed CORS origins
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
-# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
+CORS_ORIGIN_ALLOW_ALL = True
